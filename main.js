83578f990ec85a7a589f530d7caec9fa6f1fc00c
Marc <88756446+marc714@users.noreply.github.com>
2023-10-15 20:34:48 -0700

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+function windowSize() {
+    let h = window.innerHeight;
+    let w = window.innerWidth;
+
+    let resolutionWindow = document.querySelector('.resolution')
+    resolutionWindow.textContent = "Width: " + w + "x Height: " + h
+}
+
+
+
+
